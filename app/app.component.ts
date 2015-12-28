b83b42541edd477cec927a1047de4e338adc0447
paul.lessing <paul@paullessing.com>
2015-12-28 11:18:24 +0100

    Set up App Component

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core';
+
+/**
+ * Main app component for the character sheet app
+ */
+@Component({
+    selector: 'character-sheet',
+    template: '<h1>D&D 5e Character Sheet</h1>'
+})
+class AppComponent {
+
+}
+
+
+export {
+    AppComponent
+}
